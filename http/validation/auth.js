f19e82c3e2259efbb2b9aae6b74b1bf79bd95cf5
Bahrom554 <bahrombozorboev554@gmail.com>
2024-06-24 13:27:00 +0500

    validation changed

--- http/validation/auth.js
+++ http/validation/auth.js
@@ -5,7 +5,7 @@ const authSchemas = {
         password: Joi.string().required().max(128)
     }),
     register: Joi.object().keys({
-        phone: Joi.string().pattern(/^\+[0-9]+$/).required(),
+        phone: Joi.string().pattern(/^[0-9]+$/).required(),
         id: Joi.number().required(),
         name: Joi.string().required(),
         username: Joi.string().optional().allow(null, ''),
